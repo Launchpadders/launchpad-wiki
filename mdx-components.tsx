87690eaa011b0367c0c38b8f2837d06ce86a0926
trobonox <57040351+trobonox@users.noreply.github.com>
2026-09-22 21:45:59 +0200

    Unslop, prepare for community contributions

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,13 +1,11 @@
 import defaultMdxComponents from 'fumadocs-ui/mdx';
 import type { MDXComponents } from 'mdx/types';
 import { AlsUploadAnalyzer } from '@/app/components/AlsUploadAnalyzer';
-import { LaunchpadSimulator } from '@/app/components/launchpad/LaunchpadSimulator';
 
 export function getMDXComponents(components?: MDXComponents): MDXComponents {
   return {
     ...defaultMdxComponents,
     AlsUploadAnalyzer,
-    LaunchpadSimulator,
     ...components,
   };
 }
